fbb10d1af9f9a37bece020efeb5890bc8ef74bce
Kristine Erickson <ericksonk09@gmail.com>
2017-07-18 13:13:34 -0400

    Set up component test

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,9 @@ module.exports = {
         ],
         "react/jsx-filename-extension": [
           1, { "extensions": [".js", ".jsx"] }
+        ],
+        "import/no-extraneous-dependencies": [
+          2, { devDependencies: true }
         ]
     }
 };
